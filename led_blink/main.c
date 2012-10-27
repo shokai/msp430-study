fbf7ff38240fa2f4f39cd575a788f342e3bee558
Sho Hashimoto <hashimoto@shokai.org>
2012-10-27 20:04:32 +0900

    blink external LED

--- led_blink/main.c
+++ led_blink/main.c
@@ -3,6 +3,7 @@
 
 #define LED0 BIT0 // 0b00000001
 #define LED1 BIT6 // 0b01000000
+#define LED2 BIT7
 
 #define LED0_ON P1OUT |= LED0
 #define LED0_OFF P1OUT &=~ LED0
@@ -10,6 +11,9 @@
 #define LED1_ON P1OUT |= LED1
 #define LED1_OFF P1OUT &=~ LED1
 #define LED1_TOGGLE P1OUT ^= LED1
+#define LED2_ON P1OUT |= LED2
+#define LED2_OFF P1OUT &=~ LED2
+#define LED2_TOGGLE P1OUT ^= LED2
 
 /** wait 1 sec **/
 void delay(unsigned int d) {
@@ -28,14 +32,16 @@ void delay(unsigned int d) {
 
 int main(void) {
   WDTCTL = WDTPW + WDTHOLD;
-  P1DIR |= LED0 + LED1;
+  P1DIR |= LED0 + LED1 + LED2;
   
   while(1) {
     LED0_ON;
     LED1_OFF;
+    LED2_TOGGLE;
     delay(1);
     LED0_OFF;
     LED1_ON;
+    LED2_TOGGLE;
     delay(1);
   }
 }
